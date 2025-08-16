8627fadac5eeb5c77b131d4e133038e41559b5ab
hoingut <juddojibon5@gmail.com>
2025-08-16 14:53:39 +0600

    Create firebaseConfig.js

---
+++ static/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3F8gSkk6J9ChGRVB3_8DQP7FpBCl2T-w",
+    authDomain: "anyshop-1f435.firebaseapp.com",
+    projectId: "anyshop-1f435",
+    storageBucket: "anyshop-1f435.firebasestorage.app",
+    messagingSenderId: "710084687311",
+    appId: "1:710084687311:web:5320f0c91b4fb3fe35ceba"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
