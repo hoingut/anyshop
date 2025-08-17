d08c4b565339a8c51170487f86fe4445b944c541
hoingut <juddojibon5@gmail.com>
2025-08-17 10:16:45 +0600

    Update firebaseConfig.js

--- static/firebaseConfig.js
+++ static/firebaseConfig.js
@@ -1,6 +1,4 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#// Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
