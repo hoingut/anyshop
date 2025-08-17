b93a11a5a34482f0b2873338ed7754209cb2cea5
hoingut <juddojibon5@gmail.com>
2025-08-17 12:27:14 +0600

    Update firebaseConfig.js

--- static/firebaseConfig.js
+++ static/firebaseConfig.js
@@ -1,8 +1,11 @@
 
+
   // https://firebase.google.com/docs/web/setup#// Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+
 
 
   // Your web app's Firebase configuration
@@ -15,10 +18,28 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-
     appId: "1:710084687311:web:5320f0c91b4fb3fe35ceba"
   };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 // Export the services you'll need in other files
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+
+
+// Export firestore functions that will be used across the app
+export {
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  addDoc,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
